0475ba95987b4fb8ad371537fc9c2f8264145ed1
Badhia <byuneskatz@flowwest.com>
2024-03-28 13:44:05 -0700

    creates metadata files to begin drafting

---
+++ data-raw/clean_data.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(googleCloudStorageR)
+
+
+# google cloud set up
+gcs_auth(json_file = Sys.getenv("GCS_AUTH_FILE"))
+gcs_global_bucket(bucket = Sys.getenv("GCS_DEFAULT_BUCKET"))
+
+# get data from google cloud
+gcs_get_object(object_name = "juvenile-rearing-monitoring/seine-and-snorkel-data/feather-river/data/combined_feather_snorkel_data.csv",
+               bucket = gcs_get_global_bucket(),
+               saveToDisk =  here::here("data-raw", ""),
+               overwrite = TRUE)
+
+combined_snorkel <- read_csv(here::here("data-raw", ""))
+
+# clean data --------------------------------------------------------------
+# here is where we clean up the data and make sure it all looks as expected
+# check unique values for each column
+# check that all fields are being read in the right way
+
+
+
+# write files -------------------------------------------------------------
+
+# save cleaned data to `data/`
+write.csv(data_name, here::here("data", ""), row.names = FALSE)
+
