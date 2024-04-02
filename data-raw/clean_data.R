108b0ae7860ec3b44e6ff48cc2a078a6d77d4d36
Badhia <byuneskatz@flowwest.com>
2024-04-02 11:39:02 -0700

    updates field descriptions and deletes TODOs

--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -23,14 +23,10 @@ survey_locations <- read_csv(here::here("data", "survey_locations.csv")) |>
 # check unique values for each column
 # check that all fields are being read in the right way
 
-table(microhabitat_fish_observations$transect_code) #TODO confirm field meaning
-range(microhabitat_fish_observations$fish_data_id) #TODO confirm field meaning
 
 range(survey_locations$water_temp, na.rm = TRUE) #TODO check if makes sense to turn 0 into NA since temp rance is 47-69, and there are two 0s
 table(survey_locations$water_temp)
-table(survey_locations$reach_length)#TODO confirm is this field is in meters of feet
-table(survey_locations$reach_width)#TODO confirm is this field is in meters of feet
-table(survey_locations$channel_width) #TODO confirm is this field is in meters of feet
+
 # write files -------------------------------------------------------------
 
 # save cleaned data to `data/`
