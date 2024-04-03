1a267991b8648c4c43a9aae7bd8326dd4866ad81
Badhia <byuneskatz@flowwest.com>
2024-04-02 17:03:58 -0700

    adds info to project metadata

--- data-raw/make_metadata_xml.R
+++ data-raw/make_metadata_xml.R
@@ -40,11 +40,11 @@ dataset <- list() %>%
   add_datatable(datatable_metadata)
 
 # GO through and check on all units
-custom_units <- data.frame(id = c("river mile", "numer of divers", "NTU", "count of fish"),
+custom_units <- data.frame(id = c("river mile", "number of divers", "NTU", "count of fish"),
                            unitType = c("dimensionless", "dimensionless", "dimensionless", "dimensionless"),
-                           parentSI = c(NA, NA),
-                           multiplierToSI = c(NA, NA),
-                           description = c("river mile", "number of divers","NTU", "count of fish"))
+                           parentSI = c(NA, NA, NA, NA),
+                           multiplierToSI = c(NA, NA, NA, NA),
+                           description = c("river mile", "number of divers", "NTU", "count of fish"))
 
 
 unitList <- EML::set_unitList(custom_units)
